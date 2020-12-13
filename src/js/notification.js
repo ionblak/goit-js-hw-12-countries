a2f42301417ae8e52bcce0397535f9d7053493fd
Igor Ionasku <ionaigor10@gmail.com>
2020-12-13 17:00:34 +0300

    update style notification

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,11 +1,11 @@
-import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import { info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 export function showNotificationToMany() {
   defaultModules.set(PNotifyMobile, {});
-  error({
+  info({
     text: 'Too many matches found. Please enter a more specific query!',
   });
 }
